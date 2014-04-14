0192b57ab5afaa0234dbdb40717694278aae4a80
wangchangshuai <415939252@qq.com>
2014-04-15 00:14:48 +0800

    增加设置用户信息代码

---
+++ src/cn/edu/jlu/ccst/firstaidoflove/functions/beans/ResultCode.java
@@ -0,0 +1,89 @@
+package cn.edu.jlu.ccst.firstaidoflove.functions.beans;
+
+import org.json.JSONObject;
+
+import android.os.Bundle;
+import android.os.Parcel;
+import android.os.Parcelable;
+import cn.edu.jlu.ccst.firstaidoflove.util.AidException;
+
+public class ResultCode implements Parcelable
+{
+	public static String	KEY_RESULT_CODE	= "result_code";
+	private int				resultCode		= -1;
+
+	public ResultCode()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	public ResultCode(int resultCode)
+	{
+		super();
+		this.resultCode = resultCode;
+	}
+
+	public int getResultCode()
+	{
+		return resultCode;
+	}
+
+	public void setResultCode(int resultCode)
+	{
+		this.resultCode = resultCode;
+	}
+
+	public ResultCode parse(JSONObject object) throws AidException
+	{
+		if (object == null)
+		{
+			return null;
+		}
+		resultCode = object.optInt(ResultCode.KEY_RESULT_CODE);
+		return this;
+	}
+
+	@Override
+	public int describeContents()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void writeToParcel(Parcel dest, int flags)
+	{
+		Bundle bundle = new Bundle();
+		if (-1 != resultCode)
+		{
+			bundle.putLong(ResultCode.KEY_RESULT_CODE, resultCode);
+		}
+		dest.writeBundle(bundle);
+	}
+
+	public static final Parcelable.Creator<ResultCode>	CREATOR	= new Parcelable.Creator<ResultCode>() {
+																	@Override
+																	public ResultCode createFromParcel(
+																			Parcel in)
+																	{
+																		return new ResultCode(
+																				in);
+																	}
+
+																	@Override
+																	public ResultCode[] newArray(
+																			int size)
+																	{
+																		return new ResultCode[size];
+																	}
+																};
+
+	public ResultCode(Parcel in)
+	{
+		Bundle bundle = in.readBundle();
+		if (bundle.containsKey(ResultCode.KEY_RESULT_CODE))
+		{
+			resultCode = bundle.getInt(ResultCode.KEY_RESULT_CODE);
+		}
+	}
+}
---
+++ src/cn/edu/jlu/ccst/firstaidoflove/functions/beans/user/UserSetHelper.java
@@ -0,0 +1,115 @@
+/**
+ * $id$ Copyright 2011-2012 Aid Inc. All rights reserved.
+ */
+package cn.edu.jlu.ccst.firstaidoflove.functions.beans.user;
+
+import java.util.concurrent.Executor;
+
+import android.os.Bundle;
+import cn.edu.jlu.ccst.firstaidoflove.functions.AbstractRequestListener;
+import cn.edu.jlu.ccst.firstaidoflove.functions.beans.Aid;
+import cn.edu.jlu.ccst.firstaidoflove.functions.beans.login.LoginResponseBean;
+import cn.edu.jlu.ccst.firstaidoflove.util.AidError;
+import cn.edu.jlu.ccst.firstaidoflove.util.AidException;
+import cn.edu.jlu.ccst.firstaidoflove.util.Util;
+
+/**
+ * 
+ * @author hecao (he.cao@aid-inc.com) users.getInfo接口 助手类
+ * 
+ */
+public class UserSetHelper
+{
+	/**
+	 * aid对象
+	 */
+	private Aid	aid;
+
+	public UserSetHelper(Aid aid)
+	{
+		this.aid = aid;
+	}
+
+	/**
+	 * 同步调用users.getInfo接口<br>
+	 * 
+	 * @param param
+	 *            请求对象
+	 * @return 返回{@link LoginResponseBean}对象
+	 * @throws AidException
+	 */
+	public UserSetResponseBean setUsersInfo(UserSetRequestParam param)
+			throws AidException, Throwable
+	{
+		Bundle parameters = param.getParams();
+		UserSetResponseBean usersBean = null;
+		try
+		{
+			String response = aid.requestJSON(parameters);
+			if (response != null)
+			{
+				Util.checkResponse(response, Aid.RESPONSE_FORMAT_JSON);
+			}
+			else
+			{
+				Util.logger("null response");
+				throw new AidException(AidError.ERROR_CODE_UNKNOWN_ERROR,
+						"null response", "null response");
+			}
+			usersBean = new UserSetResponseBean(response);
+		}
+		catch (RuntimeException re)
+		{
+			Util.logger("runtime exception " + re.getMessage());
+			throw new Throwable(re);
+		}
+		return usersBean;
+	}
+
+	/**
+	 * 异步方法调用users.getInfo接口<br>
+	 * 
+	 * @param pool
+	 *            线程池
+	 * @param param
+	 *            请求对象
+	 * @param listener
+	 *            回调
+	 */
+	public void asyncsetUsersInfo(Executor pool,
+			final UserSetRequestParam param,
+			final AbstractRequestListener<UserSetResponseBean> listener)
+	{
+		pool.execute(new Runnable() {
+			@Override
+			public void run()
+			{
+				try
+				{
+					UserSetResponseBean usersBean = setUsersInfo(param);
+					if (listener != null)
+					{
+						listener.onComplete(usersBean);
+					}
+				}
+				catch (AidException e)
+				{
+					Util.logger("aid exception " + e.getMessage());
+					if (listener != null)
+					{
+						listener.onError(new AidError(e.getMessage()));
+						e.printStackTrace();
+					}
+				}
+				catch (Throwable e)
+				{
+					Util.logger("on fault " + e.getMessage());
+					if (listener != null)
+					{
+						listener.onFault(e);
+					}
+				}
+			}
+		});
+	}
+}
